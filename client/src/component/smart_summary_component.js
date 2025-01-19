fa9f5631d1339ae9ff7b5f4f49f3171867b6bc03
dakshrajput45 <dakshr050@gmail.com>
2025-01-19 21:46:12 +0530

    minor stlying fixes

--- client/src/component/smart_summary_component.js
+++ client/src/component/smart_summary_component.js
@@ -5,7 +5,7 @@ import { AppContext } from '../context/appContext';
 function SmartSummary() {
   const {summary,loadingSummary} = useContext(AppContext);
   return (
-    <div className='flex flex-col mt-2 w-full max-w-screen-lg md:h-full overflow-auto p-4 border-2 border-gray-500 shadow-md bg-white mx-auto'>
+    <div className='flex flex-col mt-2 w-full max-w-screen-lg h-[50vh] md:h-full overflow-auto p-4 border-2 border-gray-500 shadow-md bg-white mx-auto'>
       <h1 className='text-center text-xl md:text-2xl font-semibold mt-2 mb-4 bg-gray-200 p-4'>Smart Summary</h1>
       {summary && !loadingSummary ? (
         <div className='flex text-black mt-6 max-w-full'>
